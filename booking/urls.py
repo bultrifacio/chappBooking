fbf55f165982d801c64c4594387cc3d660efabfe
Juan Carlos Arroyo Herrera <jcah20022@gmail.com>
2018-10-21 12:10:38 +0100

    feat: added basic views.

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.search, name='search'),
+    path('results/', views.results, name='results'),
+    path('<int:room_id>/', views.booking, name='booking'),
+    path('reservations/', views.reservations, name='reservations'),
+    path('<int:booking_id>/', views.detail, name='detail'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-
+from django.template import loader
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world.")
+def search(request):
+    context = {'search_page': 'active'}
+    return render(request, 'booking/search.html', context)
+
+def results(request):
+    context = {}
+    return HttpResponse('Results')
+
+
+def booking(request):
+    context = {}
+    return HttpResponse('Booking')
+
+
+def reservations(request):
+    context = {'reservations_page': 'active'}
+    return render(request, 'booking/reservations.html', context)
+
+
+def detail(request):
+    context = {}
+    return HttpResponse('Details')
