ff8c11ca32ced11c68d0006f0ee4f84082b32453
Juan Carlos Arroyo Herrera <jcah20022@gmail.com>
2018-10-22 00:59:08 +0100

    refactor: refactor in some files

--- booking/urls.py
+++ booking/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name= 'booking'
 urlpatterns = [
     path('', views.search, name='search'),
     path('results/', views.results, name='results'),
-    path('<int:room_id>/', views.booking, name='booking'),
+    path('reserve/<int:room_id>/', views.reserve, name='reserve'),
     path('reservations/', views.reservations, name='reservations'),
-    path('<int:booking_id>/', views.detail, name='detail'),
+    path('detail/<int:booking_id>/', views.detail, name='detail'),
+    path('detail/<int:booking_id>/pdf', views.bookingPdf, name='bookingPdf'),
+    path('error/', views.error, name='error'),
 ]
