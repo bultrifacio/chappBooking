1722d1f507a1e9f0b04b183d27b9f9f189177e69
Juan Carlos Arroyo Herrera <jcah20022@gmail.com>
2018-10-22 00:55:05 +0100

    feat: added fields to models.

--- booking/models.py
+++ booking/models.py
@@ -4,27 +4,29 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Room(models.Model):
-    uploadPath = '.\static\booking\images\rooms'
     type_text = models.CharField(max_length=200)
     available_date = models.DateField()
     price_number = models.FloatField()
-    description_text = models.CharField(max_length=1000, default='')
+    description_text = models.CharField(max_length=1000)
 
     def __str__(self):
-        return '{type} room for {price}'.format(type=self.type_text,price=self.price_number)
+        return '{type} room, price:{price}'.format(type=self.type_text,price=self.price_number)
 
-    def is_available(self):
-        return self.available_date <= timezone.now()
 
 class Booking(models.Model):
     room = models.ForeignKey(Room, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    amount = models.FloatField()
-    days = models.IntegerField(default=1)
-    payment_card = models.IntegerField()
-    phone_number = models.CharField(max_length=20)
+    code = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    amount_number = models.FloatField()
+    days_number = models.IntegerField()
+    payment_card_text = models.CharField(max_length=20)
+    phone_number_text = models.CharField(max_length=20)
     comment_text = models.CharField(max_length=1000, null=True)
+    name_text = models.CharField(max_length=60)
+    surname_text = models.CharField(max_length=60)
+    id_card_text = models.CharField(max_length=20)
+    email = models.EmailField()
+    date = models.DateField()
 
     def __str__(self):
         return '{}'.format(self.code)
