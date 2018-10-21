91047b1f187185fe2c6dd9b3dbe5e7c1ba45d36c
Juan Carlos Arroyo Herrera <jcah20022@gmail.com>
2018-10-21 19:35:59 +0100

    feat: added the results' view of search.

--- booking/views.py
+++ booking/views.py
@@ -1,20 +1,33 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from .models import Room, Booking
 
 def search(request):
     context = {'search_page': 'active'}
     return render(request, 'booking/search.html', context)
 
+
 def results(request):
-    context = {}
-    return HttpResponse('Results')
+    try:
+        from_date = request.GET['fromDate']
+        to_date = request.GET['toDate']
+    except:
+        return redirect('booking/search.html')
+    else:
+        date_format = '%Y-%m-%d'
+        rooms = Room.objects.filter(available_date__range=[from_date, to_date])
+        from_date_formatted = datetime.strptime(from_date, date_format )
+        to_date_formatted = datetime.strptime(to_date, date_format)
+        total_days = (to_date_formatted - from_date_formatted).days
+        return render(request, 'booking/results.html', {'rooms' : rooms, 'total_days': total_days})
 
 
-def booking(request):
-    context = {}
-    return HttpResponse('Booking')
+def reserve(request, room_id):
+    room = get_object_or_404(Room, pk=room_id)
+    return render(request, 'booking/reserve.html')
 
 
 def reservations(request):
